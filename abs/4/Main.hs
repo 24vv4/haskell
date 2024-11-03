20c7abdc4344074201f1f4928e49dd91d17a7e50
y.nishizawa <you.nzawa@gmail.com>
2024-11-03 13:37:15 +0900

    refactor

--- abs/4/Main.hs
+++ abs/4/Main.hs
@@ -1,11 +1,8 @@
-judge :: (Int, Int, Int, Int) -> Int
-judge (a, b, c, x) = 
-    if 500 * a + 100 * b + 50 * c == x then 1 else 0
-
+int = readLn
 main = do
-    a <- readLn :: IO Int
-    b <- readLn :: IO Int
-    c <- readLn :: IO Int
-    x <- readLn :: IO Int
-    let l = [(na, nb, nc, x) | na <- [0..a], nb <- [0..b], nc <- [0..c], x <- [x..x]]
-    print $ sum (map judge l)
+    a <- int
+    b <- int
+    c <- int
+    x <- int
+    let l = [500 * na + 100 * nb + 50 * nc | na <- [0..a], nb <- [0..b], nc <- [0..c]]
+    print $ length $ filter (==x) l
