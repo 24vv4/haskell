a98400de5bd41cb1bb89cdf79bc489f4f4be6fb6
y.nishizawa <you.nzawa@gmail.com>
2024-11-07 22:29:08 +0900

    fix

--- typical90/4/Main.hs
+++ typical90/4/Main.hs
@@ -3,16 +3,9 @@ import Data.List.Split
 import Data.Array.IArray
 import Data.Array.Unboxed
 
-sumlist :: [Int] -> [Int] -> [Int]
-sumlist [] a = a
-sumlist a b = zipWith (+) a b
-
-fn :: [Int] -> String
-fn v = unwords . map show $ v
-
 main = do
     [h, w] <- map read . words <$> getLine :: IO [Int]
-    an <- replicateM h (map read . words <$> getLine)
+    an <- replicateM h (map read . words <$> getLine) :: IO [[Int]]
     let ar = listArray @UArray ((0, 0), (h-1, w-1)) $ concat an
     let hsum = [sum [ar ! (i, j) | j <- [0..w-1]] | i <- [0..h-1]]
     let wsum = [sum [ar ! (i, j) | i <- [0..h-1]] | j <- [0..w-1]]
@@ -22,4 +15,4 @@ main = do
     let ans = [hr ! i + wr ! j - ar ! (i, j) | i <- [0..h-1], j <- [0..w-1]]
 
     forM_ (chunksOf w ans) $ \row -> do
-        putStrLn $ fn row
+        putStrLn $ unwords . map show $ row
