61d6fb3f10bc4f5560133b976a73835093738244
y.nishizawa <you.nzawa@gmail.com>
2024-11-09 15:43:42 +0900

    add comment

--- typical90/22/unsupported.hs
+++ typical90/22/unsupported.hs
@@ -2,6 +2,7 @@ import Control.Arrow
 import qualified Data.Vector.Unboxed as VU
 import qualified Data.ByteString.Char8 as B
 
+-- This answer may be correct, but atcoder doesn't support B.readInt64 and got CE result.
 readint64 = fmap (second B.tail) . B.readInt64
 main = do
     (a,b,c) <- (\vec -> (vec VU.! 0, vec VU.! 1, vec VU.! 2)) . VU.unfoldrN 3 readint64 <$> B.getLine
