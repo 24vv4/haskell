0b02aece8a5f745485448a0a62a1652e0bc91c9f
y.nishizawa <you.nzawa@gmail.com>
2024-11-09 15:42:14 +0900

    22/gcd

---
+++ typical90/22/unsupported.hs
@@ -0,0 +1,9 @@
+import Control.Arrow
+import qualified Data.Vector.Unboxed as VU
+import qualified Data.ByteString.Char8 as B
+
+readint64 = fmap (second B.tail) . B.readInt64
+main = do
+    (a,b,c) <- (\vec -> (vec VU.! 0, vec VU.! 1, vec VU.! 2)) . VU.unfoldrN 3 readint64 <$> B.getLine
+    let g = gcd(gcd a b) c
+    print $ a `div` g + b `div` g + c `div` g - 3
