e0db7ac00eda46313fa15caaeb92779dc454e085
y.nishizawa <you.nzawa@gmail.com>
2024-11-10 14:36:13 +0900

    33/where

--- typical90/33/Main.hs
+++ typical90/33/Main.hs
@@ -1,9 +1,9 @@
 solve h w = 
     if h == 1 || w == 1 then h * w
-    else
-        let h2 = (h+1) `div` 2
+    else h2 * w2
+        where
+            h2 = (h+1) `div` 2
             w2 = (w+1) `div` 2
-        in h2 * w2
 
 main = do
     [h, w] <- map read . words <$> getLine :: IO [Int]
