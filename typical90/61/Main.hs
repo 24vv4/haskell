e886a8e99aa6e708ad0946c698235686e408412f
y.nishizawa <you.nzawa@gmail.com>
2024-11-11 23:59:18 +0900

    61/Data.Sequence

--- typical90/61/Main.hs
+++ typical90/61/Main.hs
@@ -1,21 +1,22 @@
 import Control.Arrow
 import qualified Data.Vector.Unboxed as VU
 import qualified Data.ByteString.Char8 as B
+import qualified Data.Sequence as Seq
 
 readint = fmap (second B.tail) . B.readInt
 
 solve :: VU.Vector (Int, Int) -> [Int]
-solve = solve' VU.empty
+solve = solve' Seq.empty
     where
-        solve' :: VU.Vector Int -> VU.Vector (Int, Int) -> [Int]
+        solve' :: Seq.Seq Int -> VU.Vector (Int, Int) -> [Int]
         solve' deck nx =
             if VU.null nx then []
             else 
                 let (t, x) = nx VU.! 0
                 in case t of 
-                    1 -> solve' (VU.cons x deck) (VU.tail nx)
-                    2 -> solve' (VU.snoc deck x) (VU.tail nx)
-                    3 -> deck VU.! (x-1) : solve' deck (VU.tail nx)
+                    1 -> solve' (x Seq.<| deck) (VU.tail nx)
+                    2 -> solve' (deck Seq.|> x) (VU.tail nx)
+                    3 -> maybe 0 (\i -> i) (Seq.lookup (x-1) deck) : solve' deck (VU.tail nx)
         
 main = do
     n <- (\vec -> (vec VU.! 0)) . VU.unfoldrN 1 readint <$> B.getLine
